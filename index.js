6818ade7c2ce785691ef8423a70006f1b49d336d
tuychin <tuychin@gmail.com>
2020-10-30 00:22:05 +0300

    Added CORS

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const bodyparser = require('body-parser');
 const admin = require('firebase-admin');
 const serviceAccount = require('./serviceAccountKey.json');
 
 const app = express();
 const port = 3000;
-const notification_options = {
+const notificationOptions = {
     priority: 'high',
     timeToLive: 60 * 60 * 24
 };
@@ -15,12 +16,21 @@ admin.initializeApp({
   databaseURL: 'https://my-chat-c4f16.firebaseio.com'
 });
 
+// Enables CORS
+app.use(cors({
+  'allowedHeaders': ['sessionId', 'Content-Type'],
+  'exposedHeaders': ['sessionId'],
+  'origin': 'https://email-chat.tuychin.ru',
+  'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  'preflightContinue': false
+}));
+
 app.use(bodyparser.json());
 
 app.post('/firebase/notification', (req, res) => {
     const registrationToken = req.body.registrationToken;
     const message = req.body.message;
-    const options =  notification_options;
+    const options =  notificationOptions;
     
     admin.messaging().sendToDevice(registrationToken, message, options)
         .then(response => {
